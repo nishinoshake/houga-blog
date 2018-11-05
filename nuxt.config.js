f55252e9628077ba38a00f2a435ca2a181a03484
noplan1989 <lawson.and.7.11@gmail.com>
2018-11-06 03:27:51 +0900

    ファビコンの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,11 @@ module.exports = {
         content: 'https://blog.houga.cc/og.png?2018110601'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
+    ]
   },
   env: {
     SPACE_ID: process.env.SPACE_ID,
