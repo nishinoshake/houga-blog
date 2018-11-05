0dd6762659044fab8f8d53ef86dcc23cc1401458
noplan1989 <lawson.and.7.11@gmail.com>
2018-11-06 03:48:45 +0900

    エラー画面の追加

---
+++ pages/error/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="oops">ナニカガオカシイデス</p>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: `ナニカガオカシイデス | 邦画だってさ`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.oops {
+  margin: 2rem 0 8rem;
+  font-size: 2rem;
+  @include min {
+    font-size: 2.5rem;
+  }
+}
+</style>
